e44afce5c5e05229dd230bd7d94d2c1478de048d
GluckZhang <zhanglong3030@outlook.com>
2018-01-13 15:30:35 +0100

    inject corresponding exception in try block

--- src/main/java/uk/co/probablyfine/bytemonkey/OperationMode.java
+++ src/main/java/uk/co/probablyfine/bytemonkey/OperationMode.java
@@ -1,14 +1,58 @@
 package uk.co.probablyfine.bytemonkey;
 
-import jdk.internal.org.objectweb.asm.Opcodes;
-import jdk.internal.org.objectweb.asm.Type;
-import jdk.internal.org.objectweb.asm.tree.*;
-
 import java.util.List;
 import java.util.OptionalInt;
 import java.util.stream.IntStream;
 
+import jdk.internal.org.objectweb.asm.Opcodes;
+import jdk.internal.org.objectweb.asm.Type;
+import jdk.internal.org.objectweb.asm.tree.InsnList;
+import jdk.internal.org.objectweb.asm.tree.InsnNode;
+import jdk.internal.org.objectweb.asm.tree.LdcInsnNode;
+import jdk.internal.org.objectweb.asm.tree.MethodInsnNode;
+import jdk.internal.org.objectweb.asm.tree.MethodNode;
+import jdk.internal.org.objectweb.asm.tree.TryCatchBlockNode;
+import jdk.internal.org.objectweb.asm.tree.TypeInsnNode;
+import jdk.internal.org.objectweb.asm.tree.VarInsnNode;
+
 public enum OperationMode {
+	SCIRCUIT {
+		@Override
+        public InsnList generateByteCode(MethodNode method, AgentArguments arguments) {
+			final List<TryCatchBlockNode> tryCatchBlocks = method.tryCatchBlocks;
+			InsnList list = new InsnList();
+
+            if (tryCatchBlocks.size() == 0) return list;
+            
+//            list.add(new LdcInsnNode(3000L));
+//            list.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "java/lang/Thread", "sleep", "(J)V", false));
+//            
+//            list.add(new FieldInsnNode(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;"));
+//            list.add(new LdcInsnNode("i know why now"));
+//            list.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false));
+            
+            list.add(new LdcInsnNode(tryCatchBlocks.get(0).type));
+            list.add(new MethodInsnNode(
+                Opcodes.INVOKESTATIC,
+                "uk/co/probablyfine/bytemonkey/CreateAndThrowException",
+                "throwDirectly",
+                "(Ljava/lang/String;)V",
+                false // this is not a method on an interface
+            ));
+//            list.add(new TypeInsnNode(Opcodes.NEW, "uk/co/probablyfine/bytemonkey/testfiles/MissingPropertyException"));
+//            list.add(new InsnNode(Opcodes.DUP));
+//	        list.add(new MethodInsnNode(
+//	        	  Opcodes.INVOKESPECIAL,
+//				  "uk/co/probablyfine/bytemonkey/testfiles/MissingPropertyException",
+//				  "<init>",
+//				  "()V",
+//				  false // this is not a method on an interface
+//	        ));
+//            list.add(new InsnNode(Opcodes.ATHROW));
+            
+            return list;
+        }
+	},
     LATENCY {
         @Override
         public InsnList generateByteCode(MethodNode method, AgentArguments arguments) {
---
+++ src/main/java/uk/co/probablyfine/bytemonkey/testfiles/TryCatchTestObject.java
@@ -0,0 +1,74 @@
+package uk.co.probablyfine.bytemonkey.testfiles;
+
+import java.util.Random;
+
+public class TryCatchTestObject {
+	public String sourceIndependentTryCatch() {
+		Boolean isCacheActivated = false;
+		System.out.println("in try right away!!");
+		try {
+			System.out.println("first line in try!!");
+			String arg = getArgument();
+			String key = format(arg);
+			return getProperty(key, isCacheActivated);
+		} catch (MissingPropertyException e) {
+			System.out.println("exception occured in sourceIndependentTryCatch");
+			return "missing property";
+		}
+	}
+	
+	public String sourceDependentTryCatch() {
+		Boolean isCacheActivated = true;
+		String arg = "str_arg";
+		String key = "str_key";
+		try {
+			isCacheActivated = getCacheAvailability();
+			return getProperty(key, isCacheActivated);
+		} catch (MissingPropertyException e) {
+			System.out.println("exception occured in sourceDependentTryCatch");
+			if (isCacheActivated) {
+				return "missing property";
+			} else {
+				throw new CacheDisableException();
+			}
+		}
+	}
+	
+	public String purelyResilientTryCatch() {
+		String key = "str_key";
+		try {
+			return getPropertyFromCache(key);
+		} catch (MissingPropertyException e) {
+			System.out.println("exception occured in purelyResilientTryCatch");
+			return getPropertyFromFile(key);
+		}
+	}
+
+	private String getProperty(String key, Boolean isCacheActivated) throws MissingPropertyException {
+		return null;
+	}
+
+	private String format(String arg) throws MissingPropertyException {
+		return null;
+	}
+
+	private String getArgument() throws MissingPropertyException {
+		return null;
+	}
+	
+	private boolean getCacheAvailability() {
+		Random random = new Random();
+		return random.nextDouble() < 0.5;
+	}
+	
+	private String getPropertyFromFile(String key) {
+		return "property_from_file";
+	}
+
+	private String getPropertyFromCache(String key) throws MissingPropertyException {
+		return "property_from_cache";
+	}
+	
+    public static class CacheDisableException extends RuntimeException {
+    }
+}
