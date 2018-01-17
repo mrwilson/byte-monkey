45181919f2e0695fa6a588e4512124bdd2724282
GluckZhang <zhanglong3030@outlook.com>
2018-01-17 22:13:38 +0100

    update try-catch basic test cases, to use JUnit4 Assert

---
+++ src/test/java/uk/co/probablyfine/bytemonkey/shortcircuit/TryCatchObjectNormalTest.java
@@ -0,0 +1,14 @@
+package uk.co.probablyfine.bytemonkey.shortcircuit;
+
+import org.junit.Assert;
+import org.junit.Test;
+import uk.co.probablyfine.bytemonkey.testfiles.TryCatchTestObject;
+
+public class TryCatchObjectNormalTest {
+    @Test
+    public void normalMultipleTryCatchTest() {
+        // do not inject exceptions, multipleTryCatch() should execute smoothly
+        TryCatchTestObject tcTest = new TryCatchTestObject();
+        Assert.assertEquals(tcTest.multipleTryCatch(), "_1st line in 1st tc_1st line in 2nd tc");
+    }
+}
