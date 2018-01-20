e7fd983521bcdf2eb1248884ea64324c6bdfaeb0
GluckZhang <zhanglong3030@outlook.com>
2018-01-20 17:26:58 +0100

    set a filter in byte-monkey for SpojoTest

--- example/src/test/java/com/github/sworm/spojo/SpojoTest.java
+++ example/src/test/java/com/github/sworm/spojo/SpojoTest.java
@@ -105,7 +105,7 @@ public class SpojoTest extends BaseTestCase {
 
 	@BeforeClass
 	public static void config() {
-		AgentLoader.loadAgentClass(ByteMonkeyAgent.class.getName(), "mode:analyzetc");
+		AgentLoader.loadAgentClass(ByteMonkeyAgent.class.getName(), "mode:analyzetc,filter:com/github/sworm/spojo");
 
 		List<RuleMetadata> metadataList = new ArrayList<RuleMetadata>();
 		// MetaData definition
