52bfea50b8d2c18384b1a762b4e34d54b0b515e9
GluckZhang <zhanglong3030@outlook.com>
2018-01-20 17:27:54 +0100

    optimized the log style, automatically get *Test* test classes

--- src/main/java/uk/co/probablyfine/bytemonkey/LogTryCatchInfo.java
+++ src/main/java/uk/co/probablyfine/bytemonkey/LogTryCatchInfo.java
@@ -5,13 +5,34 @@ public class LogTryCatchInfo {
         int stackLengh = Thread.currentThread().getStackTrace().length;
         String executedClassName = Thread.currentThread().getStackTrace()[2].getClassName();
         String executedMethodName = Thread.currentThread().getStackTrace()[2].getMethodName();
-        String testClassName = Thread.currentThread().getStackTrace()[stackLengh - 1].getClassName();
-        String testMethodName = Thread.currentThread().getStackTrace()[stackLengh - 1].getMethodName();
+        String testClassInfo[] = getTestClassStackIndex();
+        String testClassName = testClassInfo[0];
+        String testMethodName = testClassInfo[1];
 
         // TryCatch Info
         System.out.println(String.format("INFO ByteMonkey try catch @ %s, %s, %s", tcPosition, executedMethodName, executedClassName));
         // Testcase Info
         System.out.println(String.format("INFO ByteMonkey testCase: %s @ %s", testMethodName, testClassName));
         System.out.println("----");
+    }
+
+    public static String[] getTestClassStackIndex() {
+        String result[] = {"NOT TEST CLASS", "KNOWN METHOD"};
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        int stackLength = stackTrace.length;
+        String className = null;
+
+        for (int i = stackLength - 1; i > 0; i--) {
+            className = stackTrace[i].getClassName();
+            String slices[] = className.split("/");
+            className = slices[slices.length - 1];
+            if (className.contains("Test")) {
+                result[0] = className;
+                result[1] = stackTrace[i].getMethodName();
+                break;
+            }
+        }
+
+        return result;
     }
 }
